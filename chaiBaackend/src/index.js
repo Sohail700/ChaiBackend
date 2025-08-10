96f1e971cf4dc567862919869088ffac639ccc33
sohail <sk22222223@gmail.com>
2025-08-10 17:04:56 +0530

    Changes to be committed: 	modified:   chaiBaackend/package-lock.json 	modified:   chaiBaackend/package.json 	modified:   chaiBaackend/src/app.js 	modified:   chaiBaackend/src/index.js 	new file:   chaiBaackend/src/utils/asyncHandler.js

--- chaiBaackend/src/index.js
+++ chaiBaackend/src/index.js
@@ -1,19 +1,31 @@
-// require('dotenv').config({path: './env'})
+//main approach
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
-// import mongoose from "mongoose";
-// import { DB_NAME } from "./constants";
-
-//main approach
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("SERVER ERROR 001 # ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB CONNECTION FAILED !", err);
+  });
 
+
+//when we use middleware we use app.use()
 // first approach
 /*
+// require('dotenv').config({path: './env'})
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
 import express from "express";
 const app = express();
 
