d65ae718b0bdc130a23bd4e246449ae9dcb05fcf
sohail <sk22222223@gmail.com>
2025-08-13 14:03:18 +0530

    Changes to be committed: 	new file:   chaiBaackend/src/middlewares/multer.middleware.js 	modified:   chaiBaackend/src/utils/couldinary.js

--- chaiBaackend/src/utils/couldinary.js
+++ chaiBaackend/src/utils/couldinary.js
@@ -1,5 +1,6 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
+import { fileURLToPath } from "url";
 
 cloudinary.config({
   cloud_name: process.CLOUDINARY_API_NAME,
@@ -7,9 +8,18 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
 });
 
-const uploadResult = cloudinary.uploader.upload(
-  "https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg",
-  {
-    public_id: "shoes",
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("file is uploaded on cloundinary", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); //remove temp file from local storage
+    console.log(error);
   }
-);
+};
+
+export { uploadOnCloudinary };
