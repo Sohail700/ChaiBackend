223cb0564f42b3bfd02fd9ee6b9428e4d35db051
sohail <sk22222223@gmail.com>
2025-08-15 00:36:05 +0530

    Changes to be committed: 	modified:   chaiBaackend/src/app.js 	new file:   chaiBaackend/src/controllers/user.controller.js 	new file:   chaiBaackend/src/routes/user.routes.js

--- chaiBaackend/src/app.js
+++ chaiBaackend/src/app.js
@@ -15,4 +15,10 @@ app.use(express.json({ limit: "19kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+//routes imports
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+app.use("api/v1/users", userRouter);
 export { app };
