80f96d24ffb14d9cbe36b1d6c2f59f5cf63902fc
sohail <sk22222223@gmail.com>
2025-08-15 23:37:31 +0530

    Changes to be committed: 	modified:   chaiBaackend/src/controllers/user.controller.js

--- chaiBaackend/src/controllers/user.controller.js
+++ chaiBaackend/src/controllers/user.controller.js
@@ -1,9 +1,9 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(500).json({
-    message: "suhail khan",
-  });
+  //first time (i.e. sign up) we have form through which we get data of user
+  //then store it in db (mongoDb and postman)
+  //after that user can login since we have users credential
 });
 
 export { registerUser };
