9ec7b4c4d97b1b76b861753bbac80f8493e87b81
sohail <sk22222223@gmail.com>
2025-08-20 13:10:17 +0530

    Implement refresh token functionality and refactor cloudinary utility

--- chaiBaackend/src/utils/cloudinary.js
+++ chaiBaackend/src/utils/cloudinary.js
@@ -12,7 +12,7 @@ const uploadOnCloudinary = async (localFilePath) => {
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
-    // console.log("file is uploaded on cloundinary", response.url);
+    // console.log("file is uploaded on cloudinary", response.url);
     fs.unlinkSync(localFilePath);
     console.log(response);
     return response;
